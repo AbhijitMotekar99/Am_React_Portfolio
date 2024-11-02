2a907e8da9f6c20f3fcb7eb61aa1889867b2f2bd
Abhijit Motekar <109235675+AbhijitMotekar99@users.noreply.github.com>
2024-11-02 22:37:04 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./home.css";
 import Social from "./Social";
 import Data from './Data';
-import ScrollDown from "./scrollDown";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
