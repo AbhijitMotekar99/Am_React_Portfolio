2601ddc001e93536a29682ab998543ad093af5c9
Abhijit Motekar <109235675+AbhijitMotekar99@users.noreply.github.com>
2024-11-10 21:45:50 +0530

    Preloader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import React from 'react'
-import './App.css'
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Qualification from './components/qualification/Qualification'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-import ScrollUp from './components/scrollup/ScrollUp'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Qualification from './components/qualification/Qualification';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+import Preloader from './components/Preloader/PreLoader';
 
 function App() {
-  
+  const [isPreloaderVisible, setIsPreloaderVisible] = useState(true);
+
+  // Hide Preloader after 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => setIsPreloaderVisible(false), 5000);
+    return () => clearTimeout(timer); // Cleanup
+  }, []);
+
   return (
-       <>
-          <Header/>
-           
+    <>
+      {isPreloaderVisible ? (
+        <Preloader />
+      ) : (
+        <>
+          <Header />
+
           <main className='main'>
-            <Home/>
+            <Home />
             <About />
             <Skills />
             <Qualification />
             <Projects />
             <Contact />
-           
           </main>
+
           <Footer />
           <ScrollUp />
-
-       </>
-  )
+        </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
