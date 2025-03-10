c22ff53e81311a2f0006f76f39bfa681d38984c2
Abhijit Motekar <109235675+AbhijitMotekar99@users.noreply.github.com>
2025-03-10 23:19:22 +0530

    projects_updated

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,27 +1,47 @@
 import React from 'react'
-import Image1 from "../../assets/RealEstate.png";
-import Image2 from "../../assets/portfolio.png";
-import Image3 from "../../assets/MsDhoni.jpg";
-import Image4 from "../../assets/ToDoList.png";
-import Image5 from "../../assets/Calculator.jpg";
-import Image6 from "../../assets/TempConverter.png";
-import Image7 from "../../assets/AmazonClone.png";
-import Image8 from "../../assets/ShoeMart.png";
-import Image9 from "../../assets/Authentication.jpg";
+import Image1 from "../../assets/HFP.png";
+import Image2 from "../../assets/RealEstate.png";
+import Image3 from "../../assets/EBS Web loginsignup.png";
+import Image4 from "../../assets/portfolio.png";
+import Image5 from "../../assets/MsDhoni.jpg";
+import Image6 from "../../assets/ToDoList.png";
+import Image7 from "../../assets/Calculator.jpg";
+import Image8 from "../../assets/TempConverter.png";
+import Image9 from "../../assets/AmazonClone.png";
+import Image10 from "../../assets/ShoeMart.png";
+import Image11 from "../../assets/Authentication.jpg";
 
 export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Real Estate System",
+    title: "Heart Failure Prediction",
     description:
-      "An Online Real Estate System developed using JSP, Java, and MySQL, facilitating seamless property browsing, property buying and selling transactions.",
+      "A Heart Failure Prediction System developed with ML ensemble learning models (Bagging), Python, and JavaScript, with an integrated AI-powered chatbot.",
     link1: "#Projects",
     link2: "#Projects",
   },
   {
     id: 2,
     image: Image2,
+    title: "Real Estate System",
+    description:
+      "An Online Real Estate System developed using JSP, Java, and MySQL, facilitating seamless property browsing, property buying and selling transactions.",
+    link1: "",
+    link2: "",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Electric Billing System",
+    description:
+      "An Electricity Billing System developed using Java (Core Java, Swing) and MySQL, enabling automated bill generation, customer management, and secure data storage with an interactive GUI.",
+    link1: "",
+    link2: "",
+  },
+  {
+    id: 4,
+    image: Image4,
     title: "Portfolio",
     description:
       "A sleek Portfolio showcasing my work, crafted with React for a clean, creative UI that's fully responsive across devices.",
@@ -29,8 +49,8 @@ export const Data = [
     link2: "",
  },
   {
-    id: 3,
-    image: Image3,
+    id: 5,
+    image: Image5,
     title: "Tribute Page - Ms. Dhoni",
     description:
       "A Tribute Page dedicated to the legendary cricketer MS Dhoni, featuring captivating animations and seamless responsiveness.",
@@ -38,8 +58,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/msdhoni_tribute.github.io/",
   },
   {
-    id: 4,
-    image: Image4,
+    id: 6,
+    image: Image6,
     title: "To Do List",
     description:
       "A dynamic To-Do List application crafted with CSS and JavaScript, allowing users to add, mark, delete, and edit tasks effortlessly.",
@@ -47,8 +67,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/to_do_list.github.io/",
   },
   {
-    id: 5,
-    image: Image5,
+    id: 7,
+    image: Image7,
     title: "Calculator",
     description:
       "A JavaScript-powered Calculator boasting a creative design for intuitive and efficient mathematical computations.",
@@ -56,8 +76,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/calculatorAM.github.io/",
   },
   {
-    id: 6,
-    image: Image6,
+    id: 8,
+    image: Image8,
     title: "Tempreature Converter",
     description:
       "A Temperature Converter implemented in JavaScript, enabling seamless conversion between Celsius, Fahrenheit, and Kelvin scales with user-friendly controls",
@@ -65,8 +85,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/TempreatureConverter.github.io/",
   },
   {
-    id: 7,
-    image: Image7,
+    id: 9,
+    image: Image9,
     title: "Static Amazon Clone",
     description:
       "A static Amazon Clone project featuring CSS styling to replicate the familiar design and layout of the popular e-commerce platform.",
@@ -74,8 +94,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/Amazon_HomePage_Clone/",
   },
   {
-    id: 8,
-    image: Image8,
+    id: 10,
+    image: Image10,
     title: "ShoeMart",
     description:
       "ShoeMart: A dynamic blend of static and animated elements, crafting an engaging web page experience for showcasing footwear products.",
@@ -83,8 +103,8 @@ export const Data = [
     link2: "https://abhijitmotekar99.github.io/shoeMart.github.io/",
   },
   {
-    id: 9,
-    image: Image9,
+    id: 11,
+    image: Image11,
     title: "Login Authentication",
     description:
       "A Login Authentication system built with JavaScript and Firebase, ensuring secure storage and management of user data for seamless access control.",
